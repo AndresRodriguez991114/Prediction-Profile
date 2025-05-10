8f0ab9095181f1ba2b205e315e70bd47b72cc6e5
AndresRodriguez991114 <ayesidrodriguez@ucundinamarca.edu.co>
2025-05-10 18:11:55 -0500

    graficas

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 app = Flask(__name__)
 model = pickle.load(open('model/income_model.pkl', 'rb'))
@@ -16,6 +18,16 @@ def index():
 
         features = np.array([[age, education_num, hours, sex]])
         prediction = model.predict(features)[0]
+
+        # Generar gráfica de predicción
+        fig, ax = plt.subplots()
+        bars = ['Ingreso Aproximado']
+        values = [prediction]
+        ax.bar(bars, values)
+        ax.set_ylabel('Pesos Colombianos')
+        ax.set_title('Resultado de la Predicción')
+        plt.savefig('static/prediction_plot.png')
+        plt.close()
 
     return render_template('index.html', prediction=prediction)
 
