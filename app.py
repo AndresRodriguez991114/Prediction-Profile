1907a1b73672e54f64b2f8331f0255afae30eeaa
AndresRodriguez991114 <ayesidrodriguez@ucundinamarca.edu.co>
2025-05-10 17:28:38 -0500

    app.py

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
+
+# Cargar el modelo entrenado
 model = pickle.load(open('model/income_model.pkl', 'rb'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     prediction = None
     if request.method == 'POST':
+        # Obtener los datos del formulario
         age = int(request.form['age'])
         education_num = int(request.form['education_num'])
         hours = int(request.form['hours'])
-        occupation = request.form['occupation']
-        marital_status = request.form['marital_status']
-        sex = request.form['sex']
+        sex = 1 if request.form['sex'] == 'Male' else 0
 
-        # Simplificación: codificación dummy simulada
-        features = np.array([[age, education_num, hours, 1 if sex == 'Male' else 0]])
+        # Preparar los datos para el modelo
+        features = np.array([[age, education_num, hours, sex]])
         prediction = model.predict(features)[0]
 
     return render_template('index.html', prediction=prediction)
